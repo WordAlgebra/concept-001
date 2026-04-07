d08eadcea549a6929f8de0cd917e369f1a47029c
Jacob Gordon <bjacobgordon@gmail.com>
2026-04-07 17:24:39 -0500

    step: flips condition for user region

--- example.ts
+++ example.ts
@@ -42,12 +42,12 @@ function isPlacableBy(
         }
         else
         {
-          if ((givenUser.region !== "EU")) {
-            return givenOrder.items.every($0 => ($0.price >= 0));
+          if (!(givenUser.region !== "EU")) {
+            return (givenOrder.currency === "EUR")
           }
           else
           {
-            return (givenOrder.currency === "EUR")
+            return givenOrder.items.every($0 => ($0.price >= 0));
           }
         }
       }
