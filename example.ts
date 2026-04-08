85b1fef0b96f90171109148700265d83e9d3c53f
Jacob Gordon <bjacobgordon@gmail.com>
2026-04-07 23:01:11 -0500

    step: converts `.reduce` to `.every`

--- example.ts
+++ example.ts
@@ -31,7 +31,7 @@ function isPlacableBy(
       if (givenOrder.amount > 1000) {
         if (!givenOrder.hasDiscount) {
           if (givenUser.region !== "EU") {
-            const everyItemHasValidPrice = givenOrder.items.reduce((runningVerdict, eachItem) => runningVerdict && (eachItem.price >= 0), true);
+            const everyItemHasValidPrice = givenOrder.items.every((eachItem) => (eachItem.price >= 0));
             return everyItemHasValidPrice;
           } else {
             return (givenOrder.currency === "EUR")
