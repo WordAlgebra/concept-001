811555cb10fe941a22445edb518f5f6a6756ad5d
Jacob Gordon <bjacobgordon@gmail.com>
2026-05-06 21:08:31 -0500

    step: pins static condition in second branch

--- example.ts
+++ example.ts
@@ -36,7 +36,7 @@ function isApprovable(
       return givenUser.isAdmin;
     }
     else
-    if (givenUser.isPremium) {
+    if (true) {
       if (givenOrder.amount > 1000) {
         if (!givenOrder.hasDiscount) {
           if (givenUser.region !== "EU") {
